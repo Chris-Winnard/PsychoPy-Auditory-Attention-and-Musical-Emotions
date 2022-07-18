59af9707b8fcd723e4da3bfab421a22fd46386a6
Chris-Winnard <74295248+Chris-Winnard@users.noreply.github.com>
2022-07-18 14:20:49 +0100

    Add files via upload

--- Part 3 Script.py
+++ Part 3 Script.py
@@ -297,8 +297,6 @@ while continueRoutine:
 for thisComponent in instructionsComponents:
     if hasattr(thisComponent, "setAutoDraw"):
         thisComponent.setAutoDraw(False)
-thisExp.addData('top_instr_txt.started', top_instr_txt.tStartRefresh)
-thisExp.addData('top_instr_txt.stopped', top_instr_txt.tStopRefresh)
 # store data for thisExp (ExperimentHandler)
 x, y = mouse_2.getPos()
 buttons = mouse_2.getPressed()
@@ -314,18 +312,6 @@ if sum(buttons):
         if obj.contains(mouse_2):
             gotValidClick = True
             mouse_2.clicked_name.append(obj.name)
-thisExp.addData('mouse_2.x', x)
-thisExp.addData('mouse_2.y', y)
-thisExp.addData('mouse_2.leftButton', buttons[0])
-thisExp.addData('mouse_2.midButton', buttons[1])
-thisExp.addData('mouse_2.rightButton', buttons[2])
-if len(mouse_2.clicked_name):
-    thisExp.addData('mouse_2.clicked_name', mouse_2.clicked_name[0])
-thisExp.addData('mouse_2.started', mouse_2.tStart)
-thisExp.addData('mouse_2.stopped', mouse_2.tStop)
-thisExp.nextEntry()
-thisExp.addData('nextButton_instruct.started', nextButton_instruct.tStartRefresh)
-thisExp.addData('nextButton_instruct.stopped', nextButton_instruct.tStopRefresh)
 # the Routine "instructions" was not non-slip safe, so reset the non-slip timer
 routineTimer.reset()
 
@@ -339,11 +325,9 @@ mm.addInput(0, startExpTrigger)
 for i in range(PART_1_OUT_CHANNELS):
     mm.setAmp(0,i,0)
 mm.setAmp(0,0,spk_volume[0])
-thisExp.addData('startExpTrigger.started', win.getFutureFlipTime(clock='now'))
 while continueRoutine and routineTimer.getTime() > 0:
     mm.out()
 mm.stop()
-thisExp.addData('startExpTrigger.stopped', win.getFutureFlipTime(clock='now'))
 routineTimer.reset()
 thisExp.nextEntry()
 # end of playing part starting trig
@@ -444,8 +428,6 @@ while continueRoutine:
 for thisComponent in ReadyComponents:
     if hasattr(thisComponent, "setAutoDraw"):
         thisComponent.setAutoDraw(False)
-thisExp.addData('top_instr_txt_3.started', top_instr_txt_3.tStartRefresh)
-thisExp.addData('top_instr_txt_3.stopped', top_instr_txt_3.tStopRefresh)
 # store data for thisExp (ExperimentHandler)
 x, y = mouse_4.getPos()
 buttons = mouse_4.getPressed()
@@ -461,18 +443,6 @@ if sum(buttons):
         if obj.contains(mouse_4):
             gotValidClick = True
             mouse_4.clicked_name.append(obj.name)
-thisExp.addData('mouse_4.x', x)
-thisExp.addData('mouse_4.y', y)
-thisExp.addData('mouse_4.leftButton', buttons[0])
-thisExp.addData('mouse_4.midButton', buttons[1])
-thisExp.addData('mouse_4.rightButton', buttons[2])
-if len(mouse_4.clicked_name):
-    thisExp.addData('mouse_4.clicked_name', mouse_4.clicked_name[0])
-thisExp.addData('mouse_4.started', mouse_4.tStart)
-thisExp.addData('mouse_4.stopped', mouse_4.tStop)
-thisExp.nextEntry()
-thisExp.addData('nextButton_R1B.started', nextButton_R1B.tStartRefresh)
-thisExp.addData('nextButton_R1B.stopped', nextButton_R1B.tStopRefresh)
 # the Routine "Ready" was not non-slip safe, so reset the non-slip timer
 routineTimer.reset()
 
@@ -645,12 +615,10 @@ for thisBlock in block:
             thisComponent.setAutoDraw(False)
     
     if attend == True:
-        block.addData('attendNote.started', attendNote.tStartRefresh)
-        block.addData('attendNote.stopped', attendNote.tStopRefresh)
+        block.addData('Attended the music')
         print("attended")
     else:
-        block.addData('dontAttendNote.started', dontAttendNote.tStartRefresh) #Will this record if it was attended or not..?
-        block.addData('dontAttendNote.stopped', dontAttendNote.tStopRefresh)
+        block.addData('Did not attend the music')
         print("not attended")
     # the Routine "trial" was not non-slip safe, so reset the non-slip timer
     routineTimer.reset()
@@ -752,8 +720,6 @@ for thisBlock in block:
     for thisComponent in QuestionBreakPauseComponents:
         if hasattr(thisComponent, "setAutoDraw"):
             thisComponent.setAutoDraw(False)
-    thisExp.addData('top_instr_txt_4.started', top_instr_txt_4.tStartRefresh)
-    thisExp.addData('top_instr_txt_4.stopped', top_instr_txt_4.tStopRefresh)
     # store data for thisExp (ExperimentHandler)
     x, y = mouse_4.getPos()
     buttons = mouse_4.getPressed()
@@ -769,18 +735,6 @@ for thisBlock in block:
             if obj.contains(mouse_4):
                 gotValidClick = True
                 mouse_4.clicked_name.append(obj.name)
-    thisExp.addData('mouse_4.x', x)
-    thisExp.addData('mouse_4.y', y)
-    thisExp.addData('mouse_4.leftButton', buttons[0])
-    thisExp.addData('mouse_4.midButton', buttons[1])
-    thisExp.addData('mouse_4.rightButton', buttons[2])
-    if len(mouse_4.clicked_name):
-        thisExp.addData('mouse_4.clicked_name', mouse_4.clicked_name[0])
-    thisExp.addData('mouse_4.started', mouse_4.tStart)
-    thisExp.addData('mouse_4.stopped', mouse_4.tStop)
-    thisExp.nextEntry()
-    thisExp.addData('nextButton_R1B.started', nextButton_R1B.tStartRefresh)
-    thisExp.addData('nextButton_R1B.stopped', nextButton_R1B.tStopRefresh)
     # the Routine "QuestionBreakPause" was not non-slip safe, so reset the non-slip timer
     routineTimer.reset()
     
@@ -795,11 +749,9 @@ mm.addInput(0, stopExpTrigger)
 for i in range(PART_1_OUT_CHANNELS):
     mm.setAmp(0,i,0)
 mm.setAmp(0,0,spk_volume[0])
-thisExp.addData('stopExpTrigger.started', win.getFutureFlipTime(clock='now'))
 while continueRoutine and routineTimer.getTime() > 0:
     mm.out()
 mm.stop()
-thisExp.addData('stopExpTrigger.stopped', win.getFutureFlipTime(clock='now'))
 routineTimer.reset()
 thisExp.nextEntry()
 # End of playing part stop trig
