8466307794377f1a795868f06f24585e1ce39395
Chris-Winnard <74295248+Chris-Winnard@users.noreply.github.com>
2023-04-13 15:26:58 +0200

    Add files via upload

--- Part 3 Script.py
+++ Part 3 Script.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from psychopy import locale_setup
-from psychopy import prefs
 from psychopy import sound, gui, visual, core, data, event, logging, clock, colors, layout
 from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
                                 STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
